a1f946346117b0332b1b0a53aa78d081a967afa2
ttseno <tiemy.seno@gmail.com>
2020-05-01 23:11:28 -0300

    :construction: Add movement script

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,61 @@
+﻿using Assets.Scripts.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{    
+    public class MovementScript 
+    {
+        private MovementConfig movement;
+
+        public MovementScript(MovementConfig movementConfig)
+        {
+            if(!movementConfig.IsValidConfig())
+            {
+                Debug.Log("invalid movement config");
+            }
+            else
+            {
+                movement = movementConfig;
+            }
+        }
+        
+        public void GetInput(PlayerScript player)
+        {
+            if(movement is null)
+            {
+
+            }
+
+            if (player.stats.IsDead) return;
+
+            if (movement.IsValidConfig())
+            {
+                if (Input.GetKeyDown(movement.RightKey))
+                {
+                    player.Move(Vector3.right);
+                }
+                else if (Input.GetKeyDown(movement.LeftKey))
+                {
+                    player.Move(Vector3.left);
+                }
+                else if (Input.GetKeyDown(movement.UpKey))
+                {
+                    player.Move(Vector3.up);
+                }
+                else if (Input.GetKeyDown(movement.DownKey))
+                {
+                    player.Move(Vector3.down);
+                }
+                else if (Input.GetKeyDown(movement.BombKey))
+                {
+                    player.CreateBomb();
+                }
+            }            
+        }        
+    }
+}
