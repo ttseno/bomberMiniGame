eefa537a43ab1521148030f6e39572807067c9df
ttseno <tiemy.seno@gmail.com>
2020-07-06 17:52:42 -0300

    💄 Add pause and game over menu

--- Assets/Scripts/Menu/InGameManager.cs
+++ Assets/Scripts/Menu/InGameManager.cs
@@ -1,20 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InGameManager : MonoBehaviour
 {
+
+    public static bool GameIsPaused = false;
+
+    public GameObject inGameMenuUI;
+    public TextMeshProUGUI menuText;
+    public Button resumeButton;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    void Pause()
+    {
+        inGameMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    
+    public void Resume()
+    {
+        inGameMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
     public void EndGame()
     {
         print("GAME OVER");
-        StartCoroutine(Restart());
+        menuText.text = "GAME OVER";
+        inGameMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        resumeButton.gameObject.SetActive(false);
     }
 
-    IEnumerator Restart()
+    public void Restart()
     {
-        yield return new WaitForSeconds(2);
-
+        Resume();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+      
+    public void GoToMainMenu()
+    {
+        Resume();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
     }
 }
