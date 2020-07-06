1af72f27d1c6ca5e840ed6f9897216abfd94349d
ttseno <tiemy.seno@gmail.com>
2020-07-06 18:11:54 -0300

    🚸 Add arrows as optional movement keys

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -25,19 +25,19 @@ namespace Assets.Scripts
         {
             if (movementConfig.IsValidConfig())
             {
-                if (Input.GetKeyDown(movementConfig.RightKey))
+                if (Input.GetKeyDown(movementConfig.RightKey) || Input.GetKeyDown(KeyCode.RightArrow))
                 {
                     player.Move(Vector3.right);
                 }
-                else if (Input.GetKeyDown(movementConfig.LeftKey))
+                else if (Input.GetKeyDown(movementConfig.LeftKey) || Input.GetKeyDown(KeyCode.LeftArrow))
                 {
                     player.Move(Vector3.left);
                 }
-                else if (Input.GetKeyDown(movementConfig.UpKey))
+                else if (Input.GetKeyDown(movementConfig.UpKey) || Input.GetKeyDown(KeyCode.UpArrow))
                 {
                     player.Move(Vector3.up);
                 }
-                else if (Input.GetKeyDown(movementConfig.DownKey))
+                else if (Input.GetKeyDown(movementConfig.DownKey) || Input.GetKeyDown(KeyCode.DownArrow))
                 {
                     player.Move(Vector3.down);
                 }
